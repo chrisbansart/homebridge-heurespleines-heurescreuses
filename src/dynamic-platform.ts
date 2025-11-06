5bee4455c862830b7f7274edbe5cb79950b50757
Chris <christophe.bansart@kdetude.com>
2025-11-06 18:36:36 +0100

    v1.0.7

--- src/dynamic-platform.ts
+++ src/dynamic-platform.ts
@@ -1,7 +1,7 @@
 import { API, DynamicPlatformPlugin, Logging, PlatformAccessory, PlatformConfig, Service, Characteristic } from 'homebridge';
 import moment from 'moment-timezone';
 
-const PLUGIN_NAME = 'homebridge Heures pleines/Heures creuses';
+const PLUGIN_NAME = 'homebridge-heurespleines-heures-creuses';
 const PLATFORM_NAME = 'HPHCPlatform';
 
 interface TimeSlot {
